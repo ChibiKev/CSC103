33137fc81775ccdd9d962824c5099942be2d2109
Kevin Chen <kchen008@citymail.cuny.edu>
2017-03-29 22:54:40 -0400

    done with  14

--- 14/maps.cpp
+++ 14/maps.cpp
@@ -51,11 +51,31 @@ int main() {
 /* TODO: try to solve the same problem *WITHOUT* using maps!  Can you do
  * it with just vectors?  How about just sets?  */
  #if 1
- vector <string> words;
-	vector <string> usedwords;
+	vector <string> words;
+	vector <string> uniquewords;
+	vector <int> break_index;
 	string s;
 	while (cin >> s){
-		for (size_t i=0; size_t
+		words.push_back(s);
+	}
+	sort (words.begin(),words.end());
+	for (size_t i=0; i!= words.size()-1;++i){
+		if (words[i+1] != words[i]){
+			uniquewords.push_back(words[i]);
+			break_index.push_back(i);
+		}
+	}
+	uniquewords.push_back(words[words.size()-1]);
+	vector <int> wordcount(1,break_index[0]+1);
+	for (size_t i=0; i!= break_index.size()-1;++i){
+		wordcount.push_back(break_index[i+1] - break_index[i]);
+	}
+	wordcount.push_back(words.size()-1-break_index[break_index.size()-1]);
+	cout << "the number of words in this text is: " << words.size() << endl;
+	cout << "number of distinct words is: " << uniquewords.size() << endl;
+	for (size_t i=0; i!= uniquewords.size();++i){
+		cout << uniquewords[i] << " occurs " << wordcount[i] << " times " << endl;
+	}
  #endif
 /* TODO: if you managed to solve the above, estimate the number of
  * instructions your program will require to run on an input of n strings.
