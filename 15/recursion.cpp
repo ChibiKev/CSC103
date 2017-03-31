c4303ab11dd7cf359ca2128a661cc81c1f3cb642
Kevin Chen <kchen008@citymail.cuny.edu>
2017-03-31 00:32:19 -0400

    all done 15

--- 15/recursion.cpp
+++ 15/recursion.cpp
@@ -56,32 +56,79 @@ int fibonacci(int number){
  * IDEA: sort the left and right halves recursively, then combine them
  * together with some kind of "merge" operation.
  * NOTE: this one might be a little tricky! */
+#if 1
 vector <int> sort(vector<int> &v){
 	if (v.size()<2){
 		return v;
 		}
 	else {
-	vector <int> vleft(0,v.size()/2);
-	vector <int> vright(v.size()/2+1,v.size());
-	vleft=sort(vleft);
-	vright=sort(vright);
+	vector <int> vleft;
+	vector <int> vright;
+		for (size_t i=0; i < v.size(); i++){
+			if (i < v.size()/2){
+				vleft.push_back(v[i]);
+			}
+			else {
+				vright.push_back(v[i]);
+			}
+		}
+	sort(vleft);
+	sort(vright);
 	vector <int> sorted;
 	size_t i=0;
 	size_t j=0;
-	while (i <=vleft.size() && j<=vright.size()){
-		if (vleft[i] <= vright[j]){
+	while (i < vleft.size() && j<vright.size()){
+		if (vleft[i] < vright[j]){
 		sorted.push_back(vleft[i]);
-		i++;
+			i++;
 		}
 		else {
 			sorted.push_back(vright[j]);
 			j++;
 		}
 	}
+	while (i < vleft.size()) sorted.push_back(vleft[i++]);
+	while (j < vright.size()) sorted.push_back(vright[j++]);
 	v=sorted;
+	}
+	return v;
 }
-return v;
+#else
+void Merge(vector<int> & vec, vector<int> & v1, vector<int> & v2) {
+    int n1 = v1.size();
+    int n2 = v2.size();
+    int p1 = 0;
+    int p2 = 0;
+    while (p1 < n1 && p2 < n2) {
+        if (v1[p1] < v2[p2]) {
+            vec.push_back(v1[p1++]);
+        } else {
+        vec.push_back(v2[p2++]);
+        }
+    }
+    while (p1 < n1) vec.push_back(v1[p1++]);
+    while (p2 < n2) vec.push_back(v2[p2++]);
 }
+
+void Sort(vector<int> & vec) {
+    int n = vec.size();
+    if (n <= 1) return;
+    vector<int> v1;
+    vector<int> v2;
+    for (int i = 0; i < n; i++) {
+        if (i < n / 2) {
+            v1.push_back(vec[i]);
+        } else {
+        v2.push_back(vec[i]);
+        }
+    }
+    Sort(v1);
+    Sort(v2);
+    vec.clear();
+    Merge(vec, v1, v2);
+}
+#endif
+
 int main(void)
 {
 	#if 0 //output of numbers
@@ -115,7 +162,7 @@ int main(void)
 		sorting.push_back(n);
 	}
 	sort(sorting);
-	for (size_t i=0; i > sorting.size(); i++) {
+	for (size_t i=0; i < sorting.size(); i++) {
 		cout << sorting[i] << " ";
 	}
 	cout << endl;
